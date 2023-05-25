41902d581ea770ebd2b30f9e6411cf441e75bcfa
Abhinav Rajesh <abhinavrajesh@github.com>
2023-05-25 16:01:04 +0530

    fix: prettier fixes

--- tests/homeRoute.test.ts
+++ tests/homeRoute.test.ts
@@ -1,24 +1,23 @@
-import type {Request, Response} from "express";
-import { homeController } from "../src/controllers/index"
+import type { Request, Response } from "express";
+import { homeController } from "../src/controllers/index";
 
 type TestResponse = {
-    viewName: string;
-    data: unknown;
-} & Response
+  viewName: string;
+  data: unknown;
+} & Response;
 
 const request = {} as Request;
 const response = {
-    viewName: "",
-    data: {},
-    render: (view: string, viewData: unknown) => {
-        response.viewName = view,
-        response.data = viewData
-    }
+  viewName: "",
+  data: {},
+  render: (view: string, viewData: unknown) => {
+    (response.viewName = view), (response.data = viewData);
+  },
 } as TestResponse;
 
-describe('GET /', () => {
-    test("should render the index view", () => {
-        homeController(request, response);
-        expect(response.viewName).toBe("pages/index.ejs")
-    })
-})
+describe("GET /", () => {
+  test("should render the index view", () => {
+    homeController(request, response);
+    expect(response.viewName).toBe("pages/index.ejs");
+  });
+});
